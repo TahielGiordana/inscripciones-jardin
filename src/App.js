fd8a1bb6dce22dafbc73a2d888bad3449978f455
TahielGiordana <tahielgiordana@gmail.com>
2022-12-22 18:56:51 -0300

    Elevo el estado del listado de alumnos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,75 @@ import "./App.css";
 import AddressList from "./components/AddressList";
 import AddressMap from "./components/AddressMap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Papa from "papaparse";
+import { calcularDistancia } from "./utils";
+
+//Plaza
+const sampleHome = {
+  lat: -34.54295340866218,
+  lon: -58.71187638934445,
+};
 
 function App() {
+  const [studentsList, setStudentsList] = useState([]);
+  const [errorsList, setErrorList] = useState([]);
+
+  const handleFileChange = async (e) => {
+    const csvFile = e.target.files[0];
+    if (csvFile) {
+      Papa.parse(csvFile, {
+        header: true,
+        complete: (results) => {
+          const data = results.data;
+          data.forEach(async (item) => {
+            const geoData = await fetch(
+              `https://apis.datos.gob.ar/georef/api/direcciones?direccion=${item["Dirección"]}&provincia=Buenos Aires&localidad=${item["Localidad"]}`
+            )
+              .then((res) => res.json())
+              .then((res) => res);
+            if (geoData.direcciones[0]) {
+              const ubicacion = geoData.direcciones[0].ubicacion; //Almacena la primera dirección obtenida
+              console.log(geoData);
+              const newEntry = {
+                nombre: item["Nombre"],
+                apellido: item["Apellido"],
+                dni: item["DNI"],
+                direccion: item["Dirección"],
+                localidad: item["Localidad"],
+                ubicacion: ubicacion,
+                distancia: calcularDistancia(
+                  sampleHome.lat,
+                  sampleHome.lon,
+                  ubicacion.lat,
+                  ubicacion.lon
+                ),
+              };
+              setStudentsList((studentsList) => [...studentsList, newEntry]);
+            } else {
+              const newError = {
+                nombre: item["Nombre"],
+                apellido: item["Apellido"],
+                dni: item["DNI"],
+                direccion: item["Dirección"],
+                localidad: item["Localidad"],
+              };
+              setErrorList((errorsList) => [...errorsList, newError]);
+            }
+          });
+        },
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <AddressList></AddressList>
-      <AddressMap></AddressMap>
+      <AddressList
+        studentsList={studentsList}
+        errorsList={errorsList}
+        onFileChange={handleFileChange}
+      ></AddressList>
+      <AddressMap studentsList={studentsList} home={sampleHome}></AddressMap>
     </div>
   );
 }
--- src/components/AddressList.js
+++ src/components/AddressList.js
@@ -1,63 +1,9 @@
 import React, { useState } from "react";
 import { Form, ListGroup } from "react-bootstrap";
-import Papa from "papaparse";
-import { calcularDistancia } from "../utils";
 
-//Plaza
-const sampleHome = {
-  lat: -34.54295340866218,
-  lon: -58.71187638934445,
-};
-
-export default function AddressList() {
-  const [studentsList, setStudentsList] = useState([]);
-  const [errorsList, setErrorList] = useState([]);
-
-  const handleFileChange = async (e) => {
-    const csvFile = e.target.files[0];
-    if (csvFile) {
-      Papa.parse(csvFile, {
-        header: true,
-        complete: (results) => {
-          const data = results.data;
-          data.forEach(async (item) => {
-            const geoData = await fetch(
-              `https://apis.datos.gob.ar/georef/api/direcciones?direccion=${item["Dirección"]}&provincia=Buenos Aires&localidad=${item["Localidad"]}`
-            )
-              .then((res) => res.json())
-              .then((res) => res);
-            if (geoData.direcciones[0]) {
-              const ubicacion = geoData.direcciones[0].ubicacion; //Almacena la primera dirección obtenida
-              console.log(geoData);
-              const newEntry = {
-                nombre: item["Nombre"],
-                apellido: item["Apellido"],
-                dni: item["DNI"],
-                direccion: item["Dirección"],
-                localidad: item["Localidad"],
-                ubicacion: ubicacion,
-                distancia: calcularDistancia(
-                  sampleHome.lat,
-                  sampleHome.lon,
-                  ubicacion.lat,
-                  ubicacion.lon
-                ),
-              };
-              setStudentsList((studentsList) => [...studentsList, newEntry]);
-            } else {
-              const newError = {
-                nombre: item["Nombre"],
-                apellido: item["Apellido"],
-                dni: item["DNI"],
-                direccion: item["Dirección"],
-                localidad: item["Localidad"],
-              };
-              setErrorList((errorsList) => [...errorsList, newError]);
-            }
-          });
-        },
-      });
-    }
+export default function AddressList(props) {
+  const handleChange = (e) => {
+    props.onFileChange(e);
   };
 
   return (
@@ -65,11 +11,11 @@ export default function AddressList() {
       <Form.Group className="mb-3">
         <Form.Label>Cargar listado:</Form.Label>
         {/* Carga del archivo csv con el listado de inscriptos*/}
-        <Form.Control type="file" accept=".csv" onChange={handleFileChange} />
+        <Form.Control type="file" accept=".csv" onChange={handleChange} />
       </Form.Group>
       <ListGroup as={"ol"} className="mb-3" numbered>
         {/*Ordeno la lista por cercanía y renderizo la lista */}
-        {studentsList
+        {props.studentsList
           .sort((a, b) => {
             return a["distancia"] - b["distancia"];
           })
@@ -86,7 +32,7 @@ export default function AddressList() {
       </ListGroup>
       <p>No se pudo validar la dirección:</p>
       <ListGroup as={"ol"}>
-        {errorsList.map((student) => (
+        {props.errorsList.map((student) => (
           <ListGroup.Item key={student["dni"]} className="text-danger">
             <p>
               <strong>{student["nombre"] + " " + student["apellido"]}</strong>
