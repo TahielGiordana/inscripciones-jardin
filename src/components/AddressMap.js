bbf14cb954ebf903cb252cd534b259fa44cf37b8
TahielGiordana <tahielgiordana@gmail.com>
2022-12-22 16:09:22 -0300

    Agrego el mapa mediante leaflet

--- src/components/AddressMap.js
+++ src/components/AddressMap.js
@@ -1,5 +1,35 @@
 import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "../react-leaflet.css";
+import L from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+//Importar icono del marcador
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
 
 export default function AddressMap() {
-  return <div></div>;
+  return (
+    <MapContainer
+      center={[-34.54295340866218, -58.71187638934445]}
+      zoom={14}
+      scrollWheelZoom={false}
+      style={{ height: "100vh" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[-34.54295340866218, -58.71187638934445]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
